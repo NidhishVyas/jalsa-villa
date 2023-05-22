4e3f7e53780affe2ef03089642e73683d8a8af2f
Nidhish Vyas <66550774+NidhishVyas@users.noreply.github.com>
2023-05-22 22:55:39 +0530

    Delete server directory

--- server/routes/admin.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const Controller = require("../controllers/admin.js");
-
-router.put("/status/:id", Controller.updateStatus);
-router.get("/pending", Controller.getPending);
-router.get("/ack", Controller.getAcknowledged);
-
-module.exports = router;
--- server/routes/index.js
+++
@@ -1,9 +0,0 @@
-const userRouter = require("./user");
-const adminRouter = require("./admin");
-
-const initRoutes = (app) => {
-  app.use("/api/v1/user", userRouter);
-  app.use("/api/v1/admin", adminRouter);
-};
-
-module.exports = initRoutes;
